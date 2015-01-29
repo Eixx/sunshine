6297472e336b461df2f78a0fdde6ea388f67c74f
Christian Petersen <cpe@visiolink.com>
2015-01-29 14:53:28 +0100

    Lesson 4a: Create Database with SQLite Open Helper - With data

--- Sunshine/app/src/main/java/com/example/cpe/sunshine/app/data/WeatherDbHelper.java
+++ Sunshine/app/src/main/java/com/example/cpe/sunshine/app/data/WeatherDbHelper.java
@@ -26,66 +26,74 @@ import com.example.cpe.sunshine.app.data.WeatherContract.WeatherEntry;
  */
 public class WeatherDbHelper extends SQLiteOpenHelper {
 
-    // If you change the database schema, you must increment the database version.
-    private static final int DATABASE_VERSION = 1;
+  // If you change the database schema, you must increment the database version.
+  private static final int DATABASE_VERSION = 1;
 
-    public static final String DATABASE_NAME = "weather.db";
+  public static final String DATABASE_NAME = "weather.db";
 
-    public WeatherDbHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
+  public WeatherDbHelper(Context context) {
+    super(context, DATABASE_NAME, null, DATABASE_VERSION);
+  }
 
-    @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        /**
-         * TODO YOUR CODE BELOW HERE FOR QUIZ
-         * QUIZ - 4a - LocationEntry
-         * https://www.udacity.com/course/viewer#!/c-ud853/l-1639338560/e-1633698599/m-1633698600
-         **/
+  @Override
+  public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    /**
+     * TODO YOUR CODE BELOW HERE FOR QUIZ
+     * QUIZ - 4a - LocationEntry
+     * https://www.udacity.com/course/viewer#!/c-ud853/l-1639338560/e-1633698599/m-1633698600
+     **/
 
-        final String SQL_CREATE_LOCATION_TABLE = "";
+    final String SQL_CREATE_LOCATION_TABLE = "CREATE TABLE " + LocationEntry.TABLE_NAME + " (" +
+        LocationEntry._ID + " INTEGER PRIMARY KEY," +
 
-        /* TODO Uncomment for
-         4a - Create a Database for SQLiteOpenHelper
-         https://www.udacity.com/course/viewer#!/c-ud853/l-1639338560/m-1633698598
-        final String SQL_CREATE_WEATHER_TABLE = "CREATE TABLE " + WeatherEntry.TABLE_NAME + " (" +
-                // Why AutoIncrement here, and not above?
-                // Unique keys will be auto-generated in either case.  But for weather
-                // forecasting, it's reasonable to assume the user will want information
-                // for a certain date and all dates *following*, so the forecast data
-                // should be sorted accordingly.
-                WeatherEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+        LocationEntry.COLOUM_LOCATION_SETTINGS + " TEXT UNIQUE NOT NULL," +
+        LocationEntry.COLOUM_CITY_NAME + " TEXT NOT NULL, " +
+        LocationEntry.COLOUM_COORD_LAT + " REAL NOT NULL, " +
+        LocationEntry.COLOUM_COORD_LONG + " REAL NOT NULL, " +
+        "UNIQUE (" + LocationEntry.COLOUM_LOCATION_SETTINGS +") ON CONFLICT IGNORE" +
+        " );";
 
-                // the ID of the location entry associated with this weather data
-                WeatherEntry.COLUMN_LOC_KEY + " INTEGER NOT NULL, " +
-                WeatherEntry.COLUMN_DATETEXT + " TEXT NOT NULL, " +
-                WeatherEntry.COLUMN_SHORT_DESC + " TEXT NOT NULL, " +
-                WeatherEntry.COLUMN_WEATHER_ID + " INTEGER NOT NULL," +
 
-                WeatherEntry.COLUMN_MIN_TEMP + " REAL NOT NULL, " +
-                WeatherEntry.COLUMN_MAX_TEMP + " REAL NOT NULL, " +
+    //4a - Create a Database for SQLiteOpenHelper
+    //https://www.udacity.com/course/viewer#!/c-ud853/l-1639338560/m-1633698598
+    final String SQL_CREATE_WEATHER_TABLE = "CREATE TABLE " + WeatherEntry.TABLE_NAME + " (" +
+        // Why AutoIncrement here, and not above?
+        // Unique keys will be auto-generated in either case.  But for weather
+        // forecasting, it's reasonable to assume the user will want information
+        // for a certain date and all dates *following*, so the forecast data
+        // should be sorted accordingly.
+        WeatherEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
 
-                WeatherEntry.COLUMN_HUMIDITY + " REAL NOT NULL, " +
-                WeatherEntry.COLUMN_PRESSURE + " REAL NOT NULL, " +
-                WeatherEntry.COLUMN_WIND_SPEED + " REAL NOT NULL, " +
-                WeatherEntry.COLUMN_DEGREES + " REAL NOT NULL, " +
+        // the ID of the location entry associated with this weather data
+        WeatherEntry.COLUMN_LOC_KEY + " INTEGER NOT NULL, " +
+        WeatherEntry.COLUMN_DATETEXT + " TEXT NOT NULL, " +
+        WeatherEntry.COLUMN_SHORT_DESC + " TEXT NOT NULL, " +
+        WeatherEntry.COLUMN_WEATHER_ID + " INTEGER NOT NULL," +
 
-                // Set up the location column as a foreign key to location table.
-                " FOREIGN KEY (" + WeatherEntry.COLUMN_LOC_KEY + ") REFERENCES " +
-                LocationEntry.TABLE_NAME + " (" + LocationEntry._ID + "), " +
+        WeatherEntry.COLUMN_MIN_TEMP + " REAL NOT NULL, " +
+        WeatherEntry.COLUMN_MAX_TEMP + " REAL NOT NULL, " +
 
-                // To assure the application have just one weather entry per day
-                // per location, it's created a UNIQUE constraint with REPLACE strategy
-                " UNIQUE (" + WeatherEntry.COLUMN_DATETEXT + ", " +
-                WeatherEntry.COLUMN_LOC_KEY + ") ON CONFLICT REPLACE);";
+        WeatherEntry.COLUMN_HUMIDITY + " REAL NOT NULL, " +
+        WeatherEntry.COLUMN_PRESSURE + " REAL NOT NULL, " +
+        WeatherEntry.COLUMN_WIND_SPEED + " REAL NOT NULL, " +
+        WeatherEntry.COLUMN_DEGREES + " REAL NOT NULL, " +
 
-        sqLiteDatabase.execSQL(SQL_CREATE_LOCATION_TABLE);
-        sqLiteDatabase.execSQL(SQL_CREATE_WEATHER_TABLE);
-        */
-    }
+        // Set up the location column as a foreign key to location table.
+        " FOREIGN KEY (" + WeatherEntry.COLUMN_LOC_KEY + ") REFERENCES " +
+        LocationEntry.TABLE_NAME + " (" + LocationEntry._ID + "), " +
 
-    @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        // To assure the application have just one weather entry per day
+        // per location, it's created a UNIQUE constraint with REPLACE strategy
+        " UNIQUE (" + WeatherEntry.COLUMN_DATETEXT + ", " +
+        WeatherEntry.COLUMN_LOC_KEY + ") ON CONFLICT REPLACE);";
+
+    sqLiteDatabase.execSQL(SQL_CREATE_LOCATION_TABLE);
+    sqLiteDatabase.execSQL(SQL_CREATE_WEATHER_TABLE);
+
+  }
+
+  @Override
+  public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
         /* TODO Uncomment for
         4a - SQLiteOpenHelper onUpgrade() method
         https://www.udacity.com/course/viewer#!/c-ud853/l-1639338560/m-1633698602
@@ -99,5 +107,5 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + WeatherEntry.TABLE_NAME);
         onCreate(sqLiteDatabase);
         */
-    }
+  }
 }
