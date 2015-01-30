16b430c8221679ec8b965ad6568278f5424b8ccd
Christian Petersen <cpe@visiolink.com>
2015-01-30 13:38:55 +0100

    Lesson 4b: Layout for new listItem

--- Sunshine/app/src/main/java/com/example/cpe/sunshine/app/ForecastFragment.java
+++ Sunshine/app/src/main/java/com/example/cpe/sunshine/app/ForecastFragment.java
@@ -8,12 +8,12 @@ package com.example.cpe.sunshine.app;
 
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -25,7 +25,10 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.example.cpe.sunshine.app.data.WeatherContract;
+
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -33,6 +36,37 @@ import java.util.ArrayList;
 public class ForecastFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
   public static final String FORECAST_CLICKED_DETAIL = "com.example.clicked.details";
+
+  private static final int FORECAST_LOADER = 0;
+  // For the forecast view we're showing only a small subset of the stored data.
+  // Specify the columns we need.
+  private static final String[] FORECAST_COLUMNS = {
+      // In this case the id needs to be fully qualified with a table name, since
+      // the content provider joins the location & weather tables in the background
+      // (both have an _id column)
+      // On the one hand, that's annoying.  On the other, you can search the weather table
+      // using the location set by the user, which is only in the Location table.
+      // So the convenience is worth it.
+      WeatherContract.WeatherEntry.TABLE_NAME + "." + WeatherContract.WeatherEntry._ID,
+      WeatherContract.WeatherEntry.COLUMN_DATETEXT,
+      WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
+      WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+      WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
+      WeatherContract.LocationEntry.COLUMN_LOCATION_SETTINGS
+  };
+
+
+  // These indices are tied to FORECAST_COLUMNS.  If FORECAST_COLUMNS changes, these
+  // must change.
+  public static final int COL_WEATHER_ID = 0;
+  public static final int COL_WEATHER_DATE = 1;
+  public static final int COL_WEATHER_DESC = 2;
+  public static final int COL_WEATHER_MAX_TEMP = 3;
+  public static final int COL_WEATHER_MIN_TEMP = 4;
+  public static final int COL_LOCATION_SETTING = 5;
+
+  private String mLocation;
+
 
   ArrayAdapter<String> adapter;
   public ForecastFragment() {
@@ -73,6 +107,41 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
   }
 
   @Override
+  public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+    // This is called when a new Loader needs to be created.  This
+    // fragment only uses one loader, so we don't care about checking the id.
+
+    // To only show current and future dates, get the String representation for today,
+    // and filter the query to return weather only for dates after or including today.
+    // Only return data after today.
+    String startDate = WeatherContract.getDbDateString(new Date());
+
+    // Sort order:  Ascending, by date.
+    String sortOrder = WeatherContract.WeatherEntry.COLUMN_DATETEXT + " ASC";
+
+    mLocation = Utility.getPreferredLocation(getActivity());
+    Uri weatherForLocationUri = WeatherContract.WeatherEntry.buildWeatherLocationWithStartDate(
+        mLocation, startDate);
+
+    // Now create and return a CursorLoader that will take care of
+    // creating a Cursor for the data being displayed.
+    return new CursorLoader(
+        getActivity(),
+        weatherForLocationUri,
+        FORECAST_COLUMNS,
+        null,
+        null,
+        sortOrder
+    );
+  }
+
+  @Override
+  public void onActivityCreated(Bundle savedInstanceState) {
+    getLoaderManager().initLoader(FORECAST_LOADER, null, this);
+    super.onActivityCreated(savedInstanceState);
+  }
+
+  @Override
   public boolean onOptionsItemSelected(MenuItem item) {
     int id = item.getItemId();
 
@@ -91,11 +160,6 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
   public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
     super.onCreateOptionsMenu(menu, inflater);
     inflater.inflate(R.menu.forecast_menu, menu);
-  }
-
-  @Override
-  public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-    return null;
   }
 
   @Override
